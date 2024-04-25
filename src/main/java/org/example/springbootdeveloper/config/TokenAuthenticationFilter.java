20db5308884eef1a4aa2f01ddbb4993957ccc8d8
lyw3447 <lyw3447@naver.com>
2024-04-25 15:53:07 +0900

    jwt 토큰 api 완료

---
+++ src/main/java/org/example/springbootdeveloper/config/TokenAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package org.example.springbootdeveloper.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.config.jwt.TokenProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+// Auth 헤더 값을 가져온 뒤 액세스 토큰이 유효하면 인증 정보 설정
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter  extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        // 요청 헤더의 Authorization 키의 값 조회
+        String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+        // 가져온 값에서 접두사 제거
+        String token = getAccessToken(authorizationHeader);
+        // 가져온 토큰이 유효한지 확인, 유효할 때는 인증 정보 설정
+        if (tokenProvider.validToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentification(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(String authorizationHeader) {
+        if(authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) {
+            return authorizationHeader.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+}
---
+++ src/main/java/org/example/springbootdeveloper/domain/RefreshToken.java
@@ -0,0 +1,34 @@
+package org.example.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 파라미터 없는 생성자 생성
+@Getter
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken RefreshToken(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
+
+}
